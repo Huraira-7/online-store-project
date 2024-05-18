357c5ae4047f000726131d6ce0e4e1cf543e15e4
Muhammad Huraira Anwer <105806743+Huraira-7@users.noreply.github.com>
2024-05-18 14:59:12 +0500

    reverse previous changes and try some other changes so that newly added photos are picked directly

--- app-backend/middlewares/uploadHandler.js
+++ app-backend/middlewares/uploadHandler.js
@@ -4,7 +4,7 @@ import path from 'path';
 //find a way to display multer Errors
 const storage = multer.diskStorage({
     destination: (req,file,callback) => {
-        callback(null,'public/images')
+        callback(null,'../app-frontend/public/images')
     },
     filename: (req,file,callback) => {
         callback(null,file.fieldname + "_" + Date.now() + path.extname(file.originalname))
