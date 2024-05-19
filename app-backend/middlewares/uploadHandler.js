9737326307c68e153b9a3246b7ca70a4c456d74c
Muhammad Huraira Anwer <105806743+Huraira-7@users.noreply.github.com>
2024-05-19 12:13:36 +0500

    trying again 2

--- app-backend/middlewares/uploadHandler.js
+++ app-backend/middlewares/uploadHandler.js
@@ -4,10 +4,17 @@ import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
+console.log(__dirname)
+console.log(path.dirname(__dirname))
+const currentDir = __dirname;
+const parentDirPath = path.resolve(currentDir, '..');
+const parentDirName = path.basename(parentDirPath);
+
+console.log(parentDirName); 
 
 //find a way to display multer Errors
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, "../app-frontend/public/images"),
+    destination: path.join(path.dirname(__dirname), "app-frontend/public/images"),
     // main?.path + "/" +  "../app-frontend/public/images",
     // (req,file,callback) => {
     //     callback(null,'../app-frontend/public/images')
